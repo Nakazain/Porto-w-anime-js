7664ce9369b8bbfa5e7f88028f29748611e88c45
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-20 21:53:09 +0700

    I still dont understand

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import { use, useEffect } from "react";
+import { animate, stagger, text } from "animejs";
+
+function App() {
+  useEffect(() => {
+    const { chars } = text.split("h2", { words: false, chars: true });
+    animate(chars, {
+      // Property keyframes
+      y: [
+        { to: "-2.75rem", ease: "outExpo", duration: 600 },
+        { to: 0, ease: "outBounce", duration: 800, delay: 100 },
+      ],
+      delay: stagger(50),
+      ease: "inOutCirc",
+      loopDelay: 1000,
+      loop: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="flex justify-center text-center">
+        <div className="flex flex-col max-w-4/5 items-center justify-center min-h-screen">
+          <h1 className="text-4xl font-bold text-white">Hi 👋 I'm Zen</h1>
+          <h2 className="mt-4 text-lg text-gray-300">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odit
+            sapiente consequuntur animi alias hic tenetur, sit ullam obcaecati
+            rem!
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
