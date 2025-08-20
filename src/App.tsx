dbc829310d4ea8b80008724e2f0df20769464b5b
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-20 22:16:46 +0700

    understanable

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import { animate, stagger, text } from "animejs";
 
 function App() {
   useEffect(() => {
-    const { chars } = text.split("h2", { words: false, chars: true });
-    animate(chars, {
+    const { words } = text.split("h2", { words: true });
+    console.log(words);
+    animate(words, {
       y: [
-        { to: "-2.75rem", ease: "outExpo", duration: 600 },
-        { to: 0, ease: "outBounce", duration: 800, delay: 100 },
+        { to: "-2.75rem", duration: 600 },
+        { to: 0, duration: 800, delay: 100 },
+        { to: "2.75rem", duration: 600 },
+        { to: 0, duration: 800, delay: 100 },
       ],
-      delay: stagger(50),
-      ease: "inOutCirc",
-      loopDelay: 1000,
+      delay: stagger(100, {from: "random"}),
+      loopDelay: 2000,
       loop: true,
     });
   }, []);
@@ -21,11 +23,9 @@ function App() {
       <div className="flex justify-center text-center">
         <div className="flex flex-col max-w-4/5 items-center justify-center min-h-screen">
           <h1 className="text-4xl font-bold text-white">Hi 👋 I'm Zen</h1>
-          <h2 className="mt-4 text-lg text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui odit
-            sapiente consequuntur animi alias hic tenetur, sit ullam obcaecati
-            rem!
-          </h2>
+          <div className="overflow-hidden">
+          <h2 className="mt-4 text-lg text-gray-300">Orang paling jenius</h2>
+          </div>
         </div>
       </div>
     </>
