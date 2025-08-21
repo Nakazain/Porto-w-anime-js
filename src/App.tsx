f8963d9bbb776331ad8e5dfa210ee9b57373fbe3
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-21 23:04:20 +0700

    Formating code

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,41 @@
 import { useEffect } from "react";
 import { animate, stagger, text } from "animejs";
 
-
 function App() {
-function fadeInLeftC(
-  dur: number,
-  staggerDelay: number,
-  waitUntil: number,
-  selectors: string
-) {
-  const { chars } = text.split(`#${selectors}`, { chars: true });
+  function fadeInLeftC(
+    dur: number,
+    staggerDelay: number,
+    waitUntil: number,
+    selectors: string
+  ) {
+    const { chars } = text.split(`#${selectors}`, { chars: true });
 
-  animate(chars, {
-    x: [{ from: "2rem" }, { to: 0, duration: dur }],
-    opacity: [{ from: 0 }, { to: 1, duration: dur }],
-    delay: stagger(staggerDelay, { start: waitUntil }),
-    duration: dur,
-  });
-}
+    animate(chars, {
+      x: [{ from: "2rem" }, { to: 0, duration: dur }],
+      opacity: [{ from: 0 }, { to: 1, duration: dur }],
+      delay: stagger(staggerDelay, { start: waitUntil }),
+      duration: dur,
+    });
+  }
 
-function fadeInUpC(
-  dur: number,
-  staggerDelay: number,
-  waitUntil: number,
-  selectors: string
-) {
-  const { chars } = text.split(`#${selectors}`, { chars: true });
+  function fadeInUpC(
+    dur: number,
+    staggerDelay: number,
+    waitUntil: number,
+    selectors: string
+  ) {
+    const { chars } = text.split(`#${selectors}`, { chars: true });
 
-  animate(chars, {
-    y: [{ from: "1rem" }, { to: 0, duration: dur }],
-    opacity: [{ from: 0 }, { to: 1, duration: dur }],
-    delay: stagger(staggerDelay, { start: waitUntil, from: "random" }),
-    duration: dur,
-  });
-}
+    animate(chars, {
+      y: [{ from: "1rem" }, { to: 0, duration: dur }],
+      opacity: [{ from: 0 }, { to: 1, duration: dur }],
+      delay: stagger(staggerDelay, { start: waitUntil, from: "random" }),
+      duration: dur,
+    });
+  }
 
   useEffect(() => {
-    fadeInLeftC(800, 100, 200, "name"); 
+    fadeInLeftC(800, 100, 200, "name");
     fadeInUpC(200, 10, 1000, "ket");
   }, []);
 
@@ -44,10 +43,10 @@ function fadeInUpC(
     <>
       <div className="flex justify-center text-center">
         <div className="flex flex-col max-w-4/5 items-center justify-center min-h-screen">
-          <h1 id="name" className="text-4xl font-bold text-white">
+          <h1 id="name" className="text-4xl font-bold text-white opacity-0">
             Hi 👋 I'm Zen
           </h1>
-          <h2 id="ket" className="mt-4 text-lg text-gray-300">
+          <h2 id="ket" className="mt-4 text-lg text-gray-300 opacity-0">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
             iusto vel enim est asperiores quia et eligendi inventore! Eius omnis
             inventore enim reiciendis deleniti quas impedit architecto
