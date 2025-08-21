39b9ad6b26c71d356a203b9ba5ebcd120fd46813
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-21 19:14:33 +0700

    Cool

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ function App() {
     console.log(words);
     animate(words, {
       y: [
-        { to: "-2.75rem", duration: 600 },
-        { to: 0, duration: 800, delay: 100 },
         { to: "2.75rem", duration: 600 },
         { to: 0, duration: 800, delay: 100 },
+      ],
+      opacity: [
+        { to: 0, duration: 600 },
+        { to: 1, duration: 800, delay: 100 },
       ],
       delay: stagger(100, {from: "random"}),
       loopDelay: 2000,
@@ -23,9 +25,7 @@ function App() {
       <div className="flex justify-center text-center">
         <div className="flex flex-col max-w-4/5 items-center justify-center min-h-screen">
           <h1 className="text-4xl font-bold text-white">Hi 👋 I'm Zen</h1>
-          <div className="overflow-hidden">
           <h2 className="mt-4 text-lg text-gray-300">Orang paling jenius</h2>
-          </div>
         </div>
       </div>
     </>
