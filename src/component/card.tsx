7f57e8220b9cb97e1c81f021f7ff648d1a9cef65
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-30 22:56:32 +0700

    Styling card

--- src/component/card.tsx
+++ src/component/card.tsx
@@ -1,11 +1,11 @@
 export default function Card() {
   return (
-    <div className="cursor-pointer bg-neutral-800 border border-neutral-700 w-96 p-4 group transition-all duration-300 rounded-2xl hover:-translate-y-2 overflow-hidden">
+    <div className="relative cursor-pointer bg-neutral-800 border border-neutral-700 w-96 p-4 group transition-all duration-300 z-10 rounded-2xl hover:-translate-y-2 hover:scale-[1.02] overflow-hidden">
       <div>
         <img className="h-48 object-cover rounded-xl" src="/tes.jpg" alt="" />
       </div>
       <div className="mt-4">
-        <p className="text-xl font-bold mb-2 text-center">Project Name</p>
+        <p className="text-xl font-bold mb-2 text-center"> Project</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quia
           ratione quaerat quibusdam. In, odit sunt quam dolorum, cumque deleniti
