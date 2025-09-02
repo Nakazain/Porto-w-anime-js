1904b0bb8e571adf8dab738d35db8c342e892b79
Nakazain <naufalkaruniawanzain@gmail.com>
2025-09-02 21:06:39 +0700

    Fix email input

--- src/component/input.tsx
+++ src/component/input.tsx
@@ -1,14 +1,24 @@
 type InputProps = {
-  text:string,
-  type:string,
-}
+  text: string;
+  type: string;
+};
 
-export default function Input({text, type} : InputProps){
+export default function Input({ text, type }: InputProps) {
   return (
-      <div className="w-full h-12 relative flex rounded-xl">
-        <input required className="peer w-full bg-transparent outline-none px-4 text-base rounded-xl border border-primary focus:shadow-md" id="address" type={type} />
-        <label className="absolute top-1/2 translate-y-[-50%] bg-neutral-950 left-4 px-2 peer-focus:top-0 peer-focus:left-3 font-light text-base peer-focus:text-sm peer-focus:text-primary peer-valid:-top-0 peer-valid:left-3 peer-valid:text-sm peer-valid:text-primary duration-150" htmlFor="address">
-          {text}</label>
-      </div>
+    <div className="w-full h-12 relative flex rounded-xl">
+      <input
+      placeholder=" "
+        required
+        className="peer w-full bg-transparent outline-none px-4 text-base rounded-xl border border-primary focus:shadow-md"
+        id="address"
+        type={type}
+      />
+      <label
+        className="absolute top-1/2 -translate-y-1/2 bg-neutral-950 left-4 px-2 peer-focus:top-0 peer-focus:left-3 font-light text-base peer-focus:text-sm peer-focus:text-primary peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-neutral-500 peer-not-placeholder-shown:top-0 peer-not-placeholder-shown:left-3 peer-not-placeholder-shown:text-sm peer-not-placeholder-shown:text-primary duration-150 peer-not-placeholder-shown:peer-invalid:text-red-500"
+        htmlFor="address"
+      >
+        {text}
+      </label>
+    </div>
   );
 }
