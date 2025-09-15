3de6d0b1d3b02c9c31941fd27fd050aeca521c3f
Nakazain <naufalkaruniawanzain@gmail.com>
2025-09-15 20:34:29 +0700

    Change toaster animation

--- src/component/Toaster.tsx
+++ src/component/Toaster.tsx
@@ -23,11 +23,13 @@ const Toaster: React.FC<ToastProps> = ({ message, type, onClose }) => {
     if (toastRef.current) {
       waapi.animate(toastRef.current, {
         opacity: [0, 1],
-        x: ['100%', '0%'],
-        duration: 300,
-        easing: 'ease-in-out',
+        x: ['150%', '0%'],
+        scale: [0.5, 1],
+        filter: ['blur(10px)', 'blur(0)'],
+        ease: 'out(3)',
+        duration: 400,
       });
-
+      
       const timer = setTimeout(() => {
         handleClose();
       }, 4000);
@@ -35,20 +37,21 @@ const Toaster: React.FC<ToastProps> = ({ message, type, onClose }) => {
       return () => clearTimeout(timer);
     }
   }, []);
-
+  
   const handleClose = () => {
     if (toastRef.current) {
       waapi.animate(toastRef.current, {
         opacity: [1, 0],
         x: ['0%', '100%'],
+        filter: ['blur(0)', 'blur(10px)'],
+        ease: 'inOut(3)',
         duration: 300,
-        easing: 'ease-in-out',
       });
       setTimeout(onClose, 350);
     }
   };
 
-  const baseClasses = "flex items-center p-4 mb-4 text-white rounded-lg shadow-lg w-full max-w-xs";
+  const baseClasses = "flex items-center p-4 mb-4 text-white rounded-sm shadow-lg w-full max-w-xs";
   const typeClasses = {
     success: "bg-green-500",
     error: "bg-red-500",
@@ -58,7 +61,7 @@ const Toaster: React.FC<ToastProps> = ({ message, type, onClose }) => {
     <div ref={toastRef} className={`${baseClasses} ${typeClasses[type]}`}>
       <div className="mr-3">{icons[type]}</div>
       <div className="flex-grow">{message}</div>
-      <button onClick={handleClose} className="ml-4 p-1 rounded-full hover:bg-black/20 focus:outline-none">
+      <button onClick={handleClose} className="ml-4 p-1 rounded-sm hover:bg-black/20 focus:outline-none">
         <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
       </button>
     </div>
