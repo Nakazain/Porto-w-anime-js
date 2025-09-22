75910b968f5195804ce227f85a2a3163fa9f58b6
Nakazain <naufalkaruniawanzain@gmail.com>
2025-09-22 21:14:55 +0700

    Deleting unnecessary

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ToastProvider } from "./context/ToastProvider";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Nakazain",
@@ -26,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-neutral-950 text-gray-100`}
+        className={`antialiased bg-neutral-950 text-gray-100`}
       >
           <ToastProvider>
         {children}
