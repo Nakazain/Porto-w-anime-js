c5fe1247eeecc4d94b0c60fc66f817de215ab594
Nakazain <naufalkaruniawanzain@gmail.com>
2025-10-05 20:54:07 +0700

    So that when you click it will open a new tab

--- src/app/component/card.tsx
+++ src/app/component/card.tsx
@@ -49,11 +49,11 @@ export default function Card({
                 d="M11 17H7q-2.075 0-3.537-1.463T2 12t1.463-3.537T7 7h4v2H7q-1.25 0-2.125.875T4 12t.875 2.125T7 15h4zm-3-4v-2h8v2zm5 4v-2h4q1.25 0 2.125-.875T20 12t-.875-2.125T17 9h-4V7h4q2.075 0 3.538 1.463T22 12t-1.463 3.538T17 17z"
               />
             </svg>
-            <a href={`https://${weblink}`}>{weblink}</a>
+            <a target="_blank" rel="noopener noreferrer" href={`https://${weblink}`}>{weblink}</a>
           </div>
         ) : null}
         <div className="flex justify-center gap-1 items-center cursor-pointer">
-          <a href={github}>
+          <a target="_blank" rel="noopener noreferrer" href={github}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
